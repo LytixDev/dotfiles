7c6512f05c8e93649d53bf63f6054b83447b2f10
Lytix <nicolaihbrand@gmail.com>
2021-07-11 18:47:16 +0200

    changes

--- Scripts/i3battery.sh
+++ Scripts/i3battery.sh
@@ -3,10 +3,18 @@ bat_data=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
 info="battery"
 case $BLOCK_BUTTON in
 		1)
-			bat_data=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
 			state=$(echo $bat_data | sed "s/.*state: //; s/\s.*//")
 			time_to=$(echo $bat_data | sed "s/.*time//; s/\spercentage.*//")
 			notify-send "Battery" "State: $state\nTime$time_to"
 			;;
 esac
-upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/^.*percentage/ {print $2}'
+
+percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/^.*percentage/ {print $2}')
+raw=$(echo $percentage | cut -f1 -d"%")
+
+if [ $raw -le 20 ]
+then
+	notify-send -u "critical" "Battery low $percentage"
+fi
+
+echo $percentage
---
+++ Scripts/i3date.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#case $BLOCK_BUTTON in
+		#1) echo "Left click action" ;;
+		#3) echo "Right click action" ;;
+#esac
+
+date | awk '/.*/ {print $3 "." $2 "("$1")"}'
