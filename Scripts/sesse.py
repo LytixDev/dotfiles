2ce025d534c75ab2d8fc9590838c6ccfdb08cab0
Nicolai Brand <nicolahb@stud.ntnu.no>
2024-06-11 22:14:32 +0200

    summer clean in Scripts

--- Scripts/sesse.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options, service_log_path='/dev/null')
-driver.get("https://analysis.sesse.net/")
-print(driver.page_source)
-driver.quit()
