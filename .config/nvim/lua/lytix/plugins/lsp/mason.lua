a695b34782493958c7c0b564d3875fe0dd9c0233
Nicolai Brand <nicolaibrand2002@gmail.com>
2025-07-04 23:06:11 +0200

    update nvim

--- .config/nvim/lua/lytix/plugins/lsp/mason.lua
+++ .config/nvim/lua/lytix/plugins/lsp/mason.lua
@@ -1,41 +1,42 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    dependencies = {
+      { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+      { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+    },
+    config = function()
+      -- import mason
+      local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+      -- import mason-lspconfig
+      local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+      local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+      -- enable mason and configure icons
+      mason.setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
         },
-      },
-    })
+      })
 
-    mason_lspconfig.setup({})
-
-    mason_tool_installer.setup({})
-    --{
-    --  ensure_installed = {
-    --    "prettier", -- prettier formatter
-    --    "stylua", -- lua formatter
-    --    "isort", -- python formatter
-    --    "black", -- python formatter
-    --    "pylint",
-    --    "eslint_d",
-    --  },
-    --}
-  end,
+      mason_lspconfig.setup({})
+      mason_tool_installer.setup({})
+      --   ensure_installed = {
+      --     "prettier", -- prettier formatter
+      --     "stylua", -- lua formatter
+      --     "isort", -- python formatter
+      --     "black", -- python formatter
+      --     "pylint",
+      --     "eslint_d",
+      --   },
+      -- })
+    end,
+  },
 }
